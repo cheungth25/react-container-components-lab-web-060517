f5b5918ba44ee952c1728ae153801f7015de54cd
cheungth25 <cheungth25@gmail.com>
2017-08-15 15:32:59 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,24 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReview = ({display_title, mpaa_rating, critics_pick, byline, headline, summary_short, publication_date, opening_date, date_updated, link, multimedia}) => {
+  return(
+    <div className='review'>
+      <p> title: {display_title}</p>
+    </div>
+  )
+}
+
+const MovieReviews = ({ reviews }) => {
+  return(
+    <div className='review-list'>
+      {reviews.map(MovieReview)}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews
